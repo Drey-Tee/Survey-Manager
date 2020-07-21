f602c632c1b88fcbb93b80b741fda24848817290
Drey-Tee <audreytetteh7@gmail.com>
2020-07-21 23:14:48 +0000

    bug fix

--- survey/forms.py
+++ survey/forms.py
@@ -1,4 +1,8 @@
-class ParticpantForm(ModelForm):
+from django.forms import ModelForm, TextInput, Select
+from app.models import SurveyParticipant
+
+
+class SurveyParticpantForm(ModelForm):
     content = forms.ModelChoiceField(
         queryset=Survey.objects.all(),
         widget=Select(attrs={'class': 'content'}),
